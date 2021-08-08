b15036a50530649814cd2a0ed1b1aa2103c2dc3b
Daniel Saxton <dsaxton@pm.me>
2021-08-08 15:16:26 -0500

    Add some tests

--- src/main.rs
+++ src/main.rs
@@ -222,3 +222,37 @@ fn sample_string_from_alphanumeric(length: usize) -> String {
         .collect();
     rand_string
 }
+
+#[test]
+fn sample_integer_respects_bounds() {
+    let lower: u64 = 10;
+    let upper: u64 = 20;
+    let mut result: u64;
+    for _ in 0..100 {
+        result = sample_integer_given_bounds(lower, upper);
+        assert!(result >= lower);
+        assert!(result < upper);
+    }
+}
+
+#[test]
+fn sample_float_respects_bounds() {
+    let lower: f64 = 10.0;
+    let upper: f64 = 20.0;
+    let mut result: f64;
+    for _ in 0..100 {
+        result = sample_float_given_bounds(lower, upper);
+        assert!(result >= lower);
+        assert!(result < upper);
+    }
+}
+
+#[test]
+fn sample_string_respects_length() {
+    let length: usize = 20;
+    let mut result: String;
+    for _ in 0..100 {
+        result = sample_string_from_alphanumeric(length);
+        assert_eq!(result.len(), length);
+    }
+}
