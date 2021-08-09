3773c992f62446b8baf3a85875f3fc6c23a0db43
Daniel Saxton <dsaxton@pm.me>
2021-08-09 14:55:50 -0500

    Refactor tests

--- src/sample.rs
+++ src/sample.rs
@@ -36,36 +36,41 @@ pub fn string_from_alphanumeric(length: usize) -> String {
         .collect()
 }
 
-#[test]
-fn integer_sampling_respects_bounds() {
-    let lower: u64 = 10;
-    let upper: u64 = 20;
-    let mut result: u64;
-    for _ in 0..100 {
-        result = integer_given_bounds(lower, upper);
-        assert!(result >= lower);
-        assert!(result < upper);
-    }
-}
+#[cfg(test)]
+mod tests {
+    use crate::sample;
 
-#[test]
-fn float_sampling_respects_bounds() {
-    let lower: f64 = 10.0;
-    let upper: f64 = 20.0;
-    let mut result: f64;
-    for _ in 0..100 {
-        result = float_given_bounds(lower, upper);
-        assert!(result >= lower);
-        assert!(result < upper);
+    #[test]
+    fn integer_sampling_respects_bounds() {
+        let lower: u64 = 10;
+        let upper: u64 = 20;
+        let mut result: u64;
+        for _ in 0..100 {
+            result = sample::integer_given_bounds(lower, upper);
+            assert!(result >= lower);
+            assert!(result < upper);
+        }
     }
-}
 
-#[test]
-fn string_sampling_respects_length() {
-    let length: usize = 20;
-    let mut result: String;
-    for _ in 0..100 {
-        result = string_from_alphanumeric(length);
-        assert_eq!(result.len(), length);
+    #[test]
+    fn float_sampling_respects_bounds() {
+        let lower: f64 = 10.0;
+        let upper: f64 = 20.0;
+        let mut result: f64;
+        for _ in 0..100 {
+            result = sample::float_given_bounds(lower, upper);
+            assert!(result >= lower);
+            assert!(result < upper);
+        }
+    }
+
+    #[test]
+    fn string_sampling_respects_length() {
+        let length: usize = 20;
+        let mut result: String;
+        for _ in 0..100 {
+            result = sample::string_from_alphanumeric(length);
+            assert_eq!(result.len(), length);
+        }
     }
 }
