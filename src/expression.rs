d0633607c2d5d7dc6ec76a9bd52556d8a22053fa
Daniel Saxton <dsaxton@pm.me>
2021-08-10 20:40:08 -0500

    Pivot again

--- src/expression.rs
+++ src/expression.rs
@@ -1,50 +1,32 @@
 #[allow(dead_code)]
 #[derive(Debug, PartialEq)]
-pub enum Pattern {
-    Composite { value: String, repetitions: usize },
-    Bracket { value: String, repetitions: usize },
-    Literal { value: String, repetitions: usize },
-    Paren { value: String, repetitions: usize },
+struct Composite {
+    value: String,
+    repetitions: usize,
 }
-
-impl Pattern {
-    #[allow(dead_code)]
-    fn to_paren(&self) -> Option<Pattern> {
-        match self {
-            Pattern::Literal {
-                value: _,
-                repetitions: _,
-            } => None,
-            Pattern::Paren {
-                value: _,
-                repetitions: _,
-            } => None,
-            Pattern::Bracket {
-                value: _,
-                repetitions: _,
-            } => None,
-            Pattern::Composite {
-                value: _,
-                repetitions: _,
-            } => None,
-        }
-    }
+#[allow(dead_code)]
+#[derive(Debug, PartialEq)]
+struct Literal {
+    value: String,
+    repetitions: usize,
+}
+#[allow(dead_code)]
+#[derive(Debug, PartialEq)]
+struct Paren {
+    value: String,
+    repetitions: usize,
+}
+#[allow(dead_code)]
+#[derive(Debug, PartialEq)]
+struct Bracket {
+    value: String,
+    repetitions: usize,
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::expression;
-
     #[test]
-    fn literal_to_paren() {
-        let result = expression::Pattern::Literal {
-            value: String::from("abc"),
-            repetitions: 1,
-        };
-        let expected = expression::Pattern::Paren {
-            value: String::from("(a|b|c)"),
-            repetitions: 1,
-        };
-        assert_eq!(result, expected);
+    fn test_fails() {
+        assert!(false);
     }
 }
