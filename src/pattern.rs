f349382de66f47ea2fb25ac56307bf92dc301284
Daniel Saxton <dsaxton@pm.me>
2021-08-11 13:44:32 -0500

    Add broken code

--- src/pattern.rs
+++ src/pattern.rs
@@ -15,10 +15,41 @@ enum PatternKind {
     Compound,
 }
 
+#[allow(dead_code)]
+#[derive(Debug, PartialEq)]
+struct ParseError {
+    message: String,
+}
+
+#[allow(dead_code)]
+impl Pattern {
+    fn parse(string: &str) -> Result<Pattern, ParseError> {
+        if string.is_empty() {
+            Ok(Pattern {
+                value: String::from(""),
+                kind: PatternKind::Literal,
+                repetitions: 1,
+            })
+        } else {
+            Err(ParseError {
+                message: String::from("Failed to parse string"),
+            })
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::pattern::{Pattern, PatternKind};
+
     #[test]
-    fn dummy() {
-        assert!(true);
+    fn parse_literal_pattern() {
+        let result = Pattern::parse("abc").unwrap();
+        let expected = Pattern {
+            value: String::from("abc"),
+            kind: PatternKind::Literal,
+            repetitions: 1,
+        };
+        assert_eq!(result, expected);
     }
 }
