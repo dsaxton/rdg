6aafe86701cca7eff62cfbbd00a7f026af723e1b
Daniel Saxton <dsaxton@pm.me>
2021-08-17 07:58:56 -0500

    Make functions top level

--- src/pattern.rs
+++ src/pattern.rs
@@ -24,14 +24,14 @@ struct ParseError;
 impl Pattern {
     // TODO: make this correct
     fn parse(string: &str) -> Result<Pattern, ParseError> {
-        if Pattern::can_parse_as_literal_kind(string) {
+        if can_parse_as_literal_kind(string) {
             return Ok(Pattern {
                 value: String::from(string),
                 kind: PatternKind::Literal,
                 repetitions: 1,
             });
         }
-        if Pattern::can_parse_as_parentheses_kind(string) {
+        if can_parse_as_parentheses_kind(string) {
             return Ok(Pattern {
                 value: String::from(string),
                 kind: PatternKind::Parentheses,
@@ -48,68 +48,69 @@ impl Pattern {
             repetitions: self.repetitions,
         }
     }
+}
 
-    fn can_parse_as_literal_kind(string: &str) -> bool {
-        let mut escaped_by_previous = false;
-        for (i, c) in string.chars().enumerate() {
-            if escaped_by_previous {
-                escaped_by_previous = false;
-                continue;
-            }
-            if Pattern::is_escape_character(c) && i < string.len() - 1 {
-                escaped_by_previous = true;
-                continue;
-            }
-            if Pattern::is_special_character(c) {
-                return false;
-            }
+fn can_parse_as_literal_kind(string: &str) -> bool {
+    let mut escaped_by_previous = false;
+    for (i, c) in string.chars().enumerate() {
+        if escaped_by_previous {
             escaped_by_previous = false;
+            continue;
         }
-        true
-    }
-
-    fn can_parse_as_parentheses_kind(string: &str) -> bool {
-        // need to pop quantifier off the end
-        if !string.starts_with('(') {
+        if is_escape_character(c) && i < string.len() - 1 {
+            escaped_by_previous = true;
+            continue;
+        }
+        if is_special_character(c) {
             return false;
         }
-        // what about pipes?
-        if string.ends_with(')') {
-            if Pattern::can_parse_as_literal_kind(&string[1..(string.len() - 1)]) {
-                return true;
-            }
-            return false;
-        }
-        // split on every unescaped pipe, remove surrounding parens and ensure each pattern is literal
-        false
+        escaped_by_previous = false;
     }
+    true
+}
 
-    fn is_special_character(character: char) -> bool {
-        "()[]{}*\\".chars().any(|c| c == character)
+fn can_parse_as_parentheses_kind(string: &str) -> bool {
+    // need to pop quantifier off the end
+    if !string.starts_with('(') {
+        return false;
     }
-
-    fn is_escape_character(character: char) -> bool {
-        character == '\\'
-    }
-
-    fn pop_quantifier(string: &str) -> (&str, Option<u8>) {
-        if !string.ends_with('}') {
-            return (string, None);
+    // what about pipes?
+    if string.ends_with(')') {
+        if can_parse_as_literal_kind(&string[1..(string.len() - 1)]) {
+            return true;
         }
-        let mut previous_was_brace = false;
-        for (reflected_idx, c) in string.chars().rev().enumerate() {
-            let idx = string.len() - reflected_idx - 1;
-            if previous_was_brace && !Pattern::is_escape_character(c) {
-                let parsed_quantifier = string[(idx + 2)..(string.len() - 1)].parse::<u8>();
-                match parsed_quantifier {
-                    Ok(value) => return (&string[..(idx + 1)], Some(value)),
-                    _ => return (string, None),
-                };
-            }
-            previous_was_brace = c == '{' && idx > 0;
-        }
-        (string, None)
+        return false;
     }
+    // split on every unescaped pipe, remove surrounding parens and ensure each pattern is literal
+    false
+}
+
+fn is_special_character(character: char) -> bool {
+    "()[]{}*\\".chars().any(|c| c == character)
+}
+
+fn is_escape_character(character: char) -> bool {
+    character == '\\'
+}
+
+#[allow(dead_code)]
+fn pop_quantifier(string: &str) -> (&str, Option<u8>) {
+    if !string.ends_with('}') {
+        return (string, None);
+    }
+    let mut previous_was_brace = false;
+    for (reflected_idx, c) in string.chars().rev().enumerate() {
+        let idx = string.len() - reflected_idx - 1;
+        if previous_was_brace && !is_escape_character(c) {
+            let parsed_quantifier = string[(idx + 2)..(string.len() - 1)].parse::<u8>();
+            match parsed_quantifier {
+                Ok(value) => return (&string[..(idx + 1)], Some(value)),
+                _ => return (string, None),
+            };
+        }
+        previous_was_brace = c == '{' && idx > 0;
+    }
+    (string, None)
 }
 
 #[cfg(test)]
@@ -120,7 +121,7 @@ mod tests {
     fn can_parse_as_literal_valid() {
         let mut result: bool;
         for s in ["abc", "abc\\(", "\\(abc\\)", "123", "\\*\\{"] {
-            result = Pattern::can_parse_as_literal_kind(s);
+            result = can_parse_as_literal_kind(s);
             assert!(result)
         }
     }
@@ -129,7 +130,7 @@ mod tests {
     fn can_parse_as_literal_invalid() {
         let mut result: bool;
         for s in ["(abc)", "[123]", "\\[123]", "abc(1|2|3)"] {
-            result = Pattern::can_parse_as_literal_kind(s);
+            result = can_parse_as_literal_kind(s);
             assert!(!result)
         }
     }
@@ -186,14 +187,14 @@ mod tests {
     #[test]
     fn check_special_characters() {
         for c in "()[]{}*\\".chars() {
-            assert!(Pattern::is_special_character(c));
+            assert!(is_special_character(c));
         }
     }
 
     #[test]
     fn check_non_special_characters() {
         for c in "AZaz09!@#".chars() {
-            assert!(!Pattern::is_special_character(c));
+            assert!(!is_special_character(c));
         }
     }
 
@@ -212,7 +213,7 @@ mod tests {
         ]
         .iter()
         {
-            result = Pattern::pop_quantifier(input);
+            result = pop_quantifier(input);
             assert_eq!(result, *expected);
         }
     }
